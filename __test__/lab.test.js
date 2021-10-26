a0d04d0a8d16e3693e017de48deec4f3188402f4
EmilyDSarani <emily.sarani@gmail.com>
2021-10-25 19:48:45 -0500

    forgot to push failing test, but it is still failing anyways

--- __test__/lab.test.js
+++ __test__/lab.test.js
@@ -1,4 +1,5 @@
 const getName = require ('../lab/name');
+const getNumbers = require ('../lab/push');
 
 describe('getName', () => {
   it('takes in name property of an object', async () => {
@@ -7,3 +8,12 @@ describe('getName', () => {
   });
 
 });
+
+describe('getNumber', () => {
+  it('takes in number property of an object and shows the push', async () => {
+    const number = getNumbers[1, 2, 3, 4];
+    expect(number).toEqual(4);   
+  });
+  
+});
+
---
+++ lab/push.js
@@ -0,0 +1,7 @@
+const getNumbers = () => {
+  const numbers = [1, 2, 3];
+
+return numbers.push(4);
+
+};
+module.exports = getNumbers;
